fe4a213a2ccc0b2bb9ba8f498f34f0c145536c75
Tim Schaub <tim.schaub@gmail.com>
2016-07-01 16:16:49 -0600

    Get XYZ ranges

--- src/xyz.js
+++ src/xyz.js
@@ -7,8 +7,19 @@ exports.getZ = function(resolution) {
   return Math.max(0, Math.ceil(Math.log(R0 / resolution) / Math.LN2));
 };
 
-exports.getResolution = function(z) {
+var getResolution = exports.getResolution = function(z) {
   return R0 / Math.pow(2, z);
+};
+
+exports.getRange = function(bbox, z) {
+  var resolution = getResolution(z);
+  var mapSize = SIZE * resolution;
+  return {
+    minX: Math.floor((bbox[0] + merc.EDGE) / mapSize),
+    minY: Math.floor((merc.EDGE - bbox[3]) / mapSize),
+    maxX: Math.ceil((bbox[2] + merc.EDGE) / mapSize) - 1,
+    maxY: Math.ceil((merc.EDGE - bbox[1]) / mapSize) - 1
+  };
 };
 
 exports.SIZE = SIZE;
--- test/xyz.spec.js
+++ test/xyz.spec.js
@@ -1,4 +1,5 @@
 var expect = require('chai').expect;
+var merc = require('../src/merc');
 var xyz = require('../src/xyz');
 
 describe('getZ()', function() {
@@ -25,5 +26,59 @@ describe('getResolution()', function() {
     expect(xyz.getResolution(1)).to.equal(xyz.R0 / 2);
     expect(xyz.getResolution(2)).to.equal(xyz.R0 / 4);
     expect(xyz.getResolution(3)).to.equal(xyz.R0 / 8);
+  });
+});
+
+describe('getRange()', function() {
+
+  it('returns a tile range given a bbox and z', function() {
+
+    // whole world
+    expect(xyz.getRange(merc.forward([-180, -90, 180, 90]), 0)).to.eql({
+      minX: 0, minY: 0, maxX: 0, maxY: 0
+    });
+
+    expect(xyz.getRange(merc.forward([-180, -90, 180, 90]), 1)).to.eql({
+      minX: 0, minY: 0, maxX: 1, maxY: 1
+    });
+
+    expect(xyz.getRange(merc.forward([-180, -90, 180, 90]), 2)).to.eql({
+      minX: 0, minY: 0, maxX: 3, maxY: 3
+    });
+
+    // northwest
+    expect(xyz.getRange(merc.forward([-180, 0, 0, 90]), 0)).to.eql({
+      minX: 0, minY: 0, maxX: 0, maxY: 0
+    });
+
+    expect(xyz.getRange(merc.forward([-180, 0, 0, 90]), 1)).to.eql({
+      minX: 0, minY: 0, maxX: 0, maxY: 0
+    });
+
+    expect(xyz.getRange(merc.forward([-180, 0, 0, 90]), 2)).to.eql({
+      minX: 0, minY: 0, maxX: 1, maxY: 1
+    });
+
+    expect(xyz.getRange(merc.forward([-180, 0, 0, 90]), 3)).to.eql({
+      minX: 0, minY: 0, maxX: 3, maxY: 3
+    });
+
+    // southeast
+    expect(xyz.getRange(merc.forward([0, -90, 180, 0]), 0)).to.eql({
+      minX: 0, minY: 0, maxX: 0, maxY: 0
+    });
+
+    expect(xyz.getRange(merc.forward([0, -90, 180, 0]), 1)).to.eql({
+      minX: 1, minY: 1, maxX: 1, maxY: 1
+    });
+
+    expect(xyz.getRange(merc.forward([0, -90, 180, 0]), 2)).to.eql({
+      minX: 2, minY: 2, maxX: 3, maxY: 3
+    });
+
+    expect(xyz.getRange(merc.forward([0, -90, 180, 0]), 3)).to.eql({
+      minX: 4, minY: 4, maxX: 7, maxY: 7
+    });
+
   });
 });
