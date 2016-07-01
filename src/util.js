1a657eabeac73c7b0f0c6f8da48a6ea3c8776913
Tim Schaub <tim.schaub@gmail.com>
2016-07-01 10:56:05 -0600

    Function for resolving dimensions

---
+++ src/util.js
@@ -0,0 +1,31 @@
+var bbox = require('./bbox');
+
+exports.resolveDimensions = function(config) {
+  var minMapBbox = config.bbox;
+  var minMapWidth = bbox.width(minMapBbox);
+  var minMapHeight = bbox.height(minMapBbox);
+  var width, height, widthResolution, heightResolution, resolution;
+  if (config.width) {
+    width = config.width;
+    widthResolution = minMapWidth / width;
+    if (config.height) {
+      height = config.height;
+      heightResolution = minMapHeight / height;
+      resolution = Math.max(heightResolution, widthResolution);
+    } else {
+      resolution = widthResolution;
+      height = minMapHeight / resolution;
+    }
+  } else if (config.height) {
+    height = config.height;
+    resolution = minMapHeight / height;
+    width = minMapWidth / resolution;
+  } else {
+    throw new Error('Map must be given at least a width or height');
+  }
+  return {
+    bbox: bbox.resize(minMapBbox, width * resolution, height * resolution),
+    width: width,
+    height: height
+  };
+};
---
+++ test/util.spec.js
@@ -0,0 +1,87 @@
+var util = require('../src/util');
+var expect = require('chai').expect;
+
+describe('resolveDimensions', function() {
+  it('resolves width and height given bbox and width', function() {
+    var bbox = [0, 0, 1000, 2000];
+    var width = 100;
+    var height = 200;
+
+    var dimensions = util.resolveDimensions({
+      bbox: bbox,
+      width: width
+    });
+
+    expect(dimensions.width).to.eql(width);
+    expect(dimensions.height).to.eql(height);
+    expect(dimensions.bbox).to.eql(bbox);
+  });
+
+  it('resolves width and height given bbox and height', function() {
+    var bbox = [0, 0, 1000, 2000];
+    var width = 100;
+    var height = 200;
+
+    var dimensions = util.resolveDimensions({
+      bbox: bbox,
+      height: height
+    });
+
+    expect(dimensions.width).to.eql(width);
+    expect(dimensions.height).to.eql(height);
+    expect(dimensions.bbox).to.eql(bbox);
+  });
+
+  it('resolves width and height given bbox, width, and height', function() {
+    var bbox = [0, 0, 1000, 2000];
+    var width = 100;
+    var height = 200;
+    var dimensions = util.resolveDimensions({
+      bbox: bbox,
+      width: width,
+      height: height
+    });
+
+    expect(dimensions.width).to.eql(width);
+    expect(dimensions.height).to.eql(height);
+    expect(dimensions.bbox).to.eql(bbox);
+  });
+
+  it('throws if no width or height', function() {
+    var call = function() {
+      util.resolveDimensions({
+        bbox: [0, 0, 1000, 2000]
+      });
+    };
+
+    expect(call).to.throw(Error, /must be given at least a width or height/);
+  });
+
+  it('extends bbox width if image aspect ratio is higher', function() {
+    var bbox = [0, 0, 1000, 2000];
+    var width = 200;
+    var height = 200;
+    var dimensions = util.resolveDimensions({
+      bbox: bbox,
+      width: width,
+      height: height
+    });
+    expect(dimensions.width).to.eql(width);
+    expect(dimensions.height).to.eql(height);
+    expect(dimensions.bbox).to.eql([-500, 0, 1500, 2000]);
+  });
+
+  it('extends bbox height if image aspect ratio is lower', function() {
+    var bbox = [0, 0, 1000, 2000];
+    var width = 50;
+    var height = 200;
+    var dimensions = util.resolveDimensions({
+      bbox: bbox,
+      width: width,
+      height: height
+    });
+    expect(dimensions.width).to.eql(width);
+    expect(dimensions.height).to.eql(height);
+    expect(dimensions.bbox).to.eql([0, -1000, 1000, 3000]);
+  });
+});
